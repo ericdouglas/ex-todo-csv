c39db44b5c9c7da59bfbb88614fcc7a9fd8855c4
Eric Douglas <eric.douglas.mail@gmail.com>
2017-09-05 08:15:33 -0300

    Refactor - read csv and parse data

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -2,6 +2,7 @@ defmodule TodoList do
   @moduledoc """
   Todo list application to work with .csv files through IEx.
   """
+  defstruct last_id: 0, todos: %{}
 
   @path_env %{dev: ["lib", "todos.csv"], test: ["lib", "todos_test.csv"]}
   @path Path.join(@path_env[Mix.env])
@@ -9,12 +10,26 @@ defmodule TodoList do
   def init do
     @path
     |> read_file!
+    |> format_to_work
   end
 
   defp read_file!(path) do
     path
     |> File.stream!
-    |> Enum.map(&String.replace(&1, "\n", ""))
-    |> to_string
+    |> Stream.map(&String.replace(&1, "\n", ""))
+  end
+
+  defp format_to_work(input) do
+    format_todos = fn(el, acc) ->
+      [id, task, date, status] = String.split(el, ",")
+      id = String.to_integer(id)
+
+      Map.put(acc, id, %Todo{id: id, task: task, date: date, status: status})
+    end
+
+    todos   = Enum.reduce(input, %{}, format_todos)
+    last_id = Map.keys(todos) |> Enum.max
+
+    %TodoList{last_id: last_id, todos: todos}
   end
 end
--- lib/todo_list_test.exs
+++ lib/todo_list_test.exs
@@ -2,7 +2,17 @@ defmodule TodoListTest do
   use ExUnit.Case
   doctest TodoList
 
-  test "if the app will load the data from the csv file correctly" do
-    assert TodoList.init == "1,Study Erlang,2018-01-01,todo"
+  test "if the app will load and parse the data from the csv file correctly" do
+    assert TodoList.init == %TodoList{
+      last_id: 1,
+      todos: %{
+        1 => %Todo{
+          id: 1,
+          task: "Study Erlang",
+          date: "2018-01-01",
+          status: "todo"
+        }
+      }
+    }
   end
 end
